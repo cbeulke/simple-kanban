b3d43e9c8786bc5a54f1d09478457e7c2cdca949
Christian Beulke <mail@beulke.net>
2016-10-12 22:12:50 +0200

    hapi integriert

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const http = require('http');
-const fs = require('fs');
+const Hapi = require('hapi');
 
-const requestHandler = function(req, res) {
-	fs.readFile('index.html', (err, file) => {
-		res.end(file);
-	});
+const server = new Hapi.Server();
+server.connection({
+	port: 3000
+});
+
+var route = {
+	path: '/',
+	method: 'GET',
+	handler: (req, res) => {
+		res.file('index.html');
+	}
 };
 
-const server = http.createServer(requestHandler);
+server.register([ require('inert') ], (err) => {
+	if(err) throw err;
 
-server.listen(3000);
+	server.route(route);	
+});
+
+
+server.start((err) => {
+	if(err) throw err;
+	console.log('server started...');
+});
