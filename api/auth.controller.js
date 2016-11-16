60568b292f33037e49e6d78b7bbf70c0daed6615
Christian Beulke <christian@Christians-MacBook-Pro.local>
2016-11-16 21:59:34 +0100

    authentifizierung an db angebunden

--- api/auth.controller.js
+++ api/auth.controller.js
@@ -1,8 +1,11 @@
 const jwt = require('jsonwebtoken');
 const Boom = require('boom');
 const config = require('../config');
+const mysql = require('mysql');
 
 module.exports = function() {
+    var connection = mysql.createConnection(config.database);
+
     var generateJWT = (id, username, scopes) => {
         return jwt.sign({
             _id: id,
@@ -16,10 +19,12 @@ module.exports = function() {
     
     return {
         login: (req, res) => {
-            if(req.payload.username === 'user') {
-                return res({
-                    token: generateJWT(1, 'user')
-                });
+            if(req.payload.username && req.payload.password) {
+                connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [ req.payload.username, req.payload.password ], (err, result) => {
+                    return res({
+                        token: generateJWT(1, 'user')
+                    });        
+                 })
             }
             return res(Boom.unauthorized('invalid user'));
         },
--- data/init.sql
+++ data/init.sql
@@ -4,5 +4,13 @@ GRANT ALL PRIVILEGES ON *.* TO 'kanban'@'%' WITH GRANT OPTION;
 CREATE TABLE tasks(
 	id int not null primary key auto_increment,
 	title varchar(255) not null,
-	status int not null)
-;
+	status int not null
+);
+
+CREATE TABLE users(
+	id int not null primary key auto_increment,
+	username varchar(45) not null,
+	password varchar(255) not null
+);
+
+INSERT INTO users(username, password) VALUES('ben', 'lol');
