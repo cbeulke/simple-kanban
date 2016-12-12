41672cefa19551f1571a0564f699e8354374afd6
Christian Beulke <mail@beulke.net>
2016-12-12 22:45:21 +0100

    docker-compose-dateien erstellt

--- data/init.sql
+++ data/init.sql
@@ -1,6 +1,3 @@
-CREATE USER 'kanban'@'%' IDENTIFIED BY 'kanban';
-GRANT ALL PRIVILEGES ON *.* TO 'kanban'@'%' WITH GRANT OPTION;
-
 CREATE TABLE tasks(
 	id int not null primary key auto_increment,
 	title varchar(255) not null,
